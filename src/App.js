9e15e4c425b26be4f30855b006a0b03b1858c256
Kara LeSueur <klesueur@kornerstonecredit.com>
2021-08-02 14:40:41 -0600

    nav formatting flexed and spaced

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <nav className="App-nav">
+          <h3 className="App-title"> Start Bootstrap </h3>
+          <ul className="nav-links">
+            <li className="link"> Services </li>
+            <li className="link"> Portfolio </li>
+            <li className="link"> About </li>
+            <li className="link"> Team </li>
+            <li className="link"> Contact </li>
+          </ul>
+        </nav>
     </div>
   );
 }
