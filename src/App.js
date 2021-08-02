147ae2ab73b702a30de8a20d896e0bf8124c68ba
Kara LeSueur <klesueur@kornerstonecredit.com>
2021-08-02 17:17:42 -0600

    landing page desktop breakpoint completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import NavBar from './components/NavBar';
+import Welcome from './components/Welcome';
 
 function App() {
   return (
     <div className="App">
         <NavBar />
+        <Welcome />
     </div>
   );
 }
---
+++ src/components/Welcome.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+const Welcome = () => {
+
+    return(
+        <body className="App-welcome">
+            <h3 className="greeting1"> Welcome To Our Studio! </h3>
+            <h1 className="greeting2"> It's nice to meet you </h1>
+            <button className="more-btn"> Tell Me More </button>
+        </body>
+    )
+};
+
+export default Welcome;
